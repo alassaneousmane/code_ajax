78e9843acb4e60d23a159255d84e6bd413c3d260
alassaneousmane <alassaneousmane071@gmail.com>
2016-11-11 02:17:01 +0100

    Update connexion_sql.php

--- connexion_sql.php
+++ connexion_sql.php
@@ -1,9 +1,9 @@
 <?php
 try {
-  $db = new PDO('mysql:host=localhost;dbname=messaging','root','');
+  $db = new PDO('mysql:host=localhost;dbname=DATABASE_NAME','DATABASE_USER','DATABASE_PASSWORD');
     $db->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'UTF8'");
   
 } catch (Exception $e) {
-  die('Error in '.$e->getMessage());
+  die('Error in '.$e->getMessage().' et la ligne '.$e->getLine());
 }
  ?>
